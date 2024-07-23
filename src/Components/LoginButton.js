057f9eef6fa6eb7b659c90536dfa2813984e9635
Nehasingh231 <nehakumari14072001@gmail.com>
2024-07-23 17:24:19 +0530

    login && logout

--- src/Components/LoginButton.js
+++ src/Components/LoginButton.js
@@ -1,42 +1,47 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { faUser, faCog, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { useAuth } from "../utils/authcontext";
 
 export default function LoginButton() {
-     const {
-        user,
-        isLoading,
-        error,
-        handleSignIn,
-        handleSignOut,
-      } = useAuth();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const { user, isLoading, error, handleSignIn, handleSignOut } = useAuth();
 
-    if(user) {
-        return <div className="flex gap-4 items-center">
-            <button onClick={handleSignOut}>
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className="relative flex items-center space-x-2 link mr-10">
+      {user ? (
+        <div className="relative">
+          <button onClick={toggleDropdown} className="focus:outline-none">
+            <img className="object-cover h-12 w-12 rounded-full" src={user?.photoURL} alt="user-image" />
+          </button>
+          {isDropdownOpen && (
+            <div className="absolute left-[-50px] mt-2 w-48 bg-white shadow-lg rounded-md">
+              <div className="flex flex-col p-2">
+                <Link to="/settings" className="flex items-center py-2 px-4 hover:bg-gray-100">
+                  <FontAwesomeIcon icon={faCog} className="mr-2" />
+                  Settings
+                </Link>
+                <button onClick={handleSignOut} className="flex items-center py-2 px-4 text-left hover:bg-gray-100">
+                  <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
                   Logout
-            </button>
-            <div className="flex">
-              <img className="object-cover h-12 w-12 rounded-full" 
-              src={user?.photoURL}
-               alt="user-image" />
+                </button>
+              </div>
             </div>
-            <div>
-              <h1>{user?.displayName}</h1>
-            </div>
-
+          )}
         </div>
-    }
-
-    return (
-        <div className="flex items-center space-x-2 link mr-10">
-        <FontAwesomeIcon icon={faUser} />
-        <Link to="" 
-        onClick={handleSignIn}
-        className="link text-gray-600 ">
-          Log In
-        </Link>
-      </div>
-    )
+      ) : (
+        <>
+          <FontAwesomeIcon icon={faUser} />
+          <button onClick={handleSignIn} className="link text-gray-600">
+            Log In
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
